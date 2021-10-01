b34b7d58c4278a499d8beb247af961c0317f8d2d
Tom Stagnaro <tomstagnaro1@gmail.com>
2021-10-01 13:45:42 -0700

    Completed create user and unit test

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,21 +4,27 @@ from django.contrib.auth import get_user_model
 
 class ModelTests(TestCase):
 
-    def test_create_user_with_email_successful(self):
-        """Test createing a new user with an email is successful"""
-        email = 'email@gmail.com'
-        password = '123'
-        user = get_user_model().objects.create_user(
-            email=email,
-            password=password
-        )
+  def test_create_user_with_email_successful(self):
+    """Test createing a new user with an email is successful"""
+    email = 'email@gmail.com'
+    password = '123'
+    user = get_user_model().objects.create_user(
+    email=email,
+    password=password
+    )
 
-        self.assertEqual(user.email, email)
-        self.assertTrue(user.check_password(password))
+    self.assertEqual(user.email, email)
+    self.assertTrue(user.check_password(password))
 
-    def test_new_user_email_normalized(self):
-        """Test the email for a new user is normalized"""
-        email = 'test@GMAIL.com'
-        user = get_user_model().objects.create_user(email, 'test123')
+  def test_new_user_email_normalized(self):
+    """Test the email for a new user is normalized"""
+    email = 'test@GMAIL.com'
+    user = get_user_model().objects.create_user(email, 'test123')
 
-        self.assertEqual(user.email, email.lower())
+    self.assertEqual(user.email, email.lower())
+
+
+  def test_new_user_invalid_email(self):
+    """Test creating user with no email raises error"""
+    with self.assertRaises(ValueError):
+      get_user_model().objects.create_user(None, 'test123')
