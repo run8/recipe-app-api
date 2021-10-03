8815209f6b17ba641734978f80a1289e59e5d26c
Tom Stagnaro <tomstagnaro1@gmail.com>
2021-10-03 15:32:58 -0700

    42 configure postresdb

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -5,10 +5,11 @@ from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
-class Commadn(BaseCommand):
-    """Django command to pause execution until databse is available"""
+class Command(BaseCommand):
+    """Django command to pause execution until database is available"""
 
     def handle(self, *args, **options):
+        """Handle the command"""
         self.stdout.write('Waiting for database...')
         db_conn = None
         while not db_conn:
@@ -18,4 +19,4 @@ class Commadn(BaseCommand):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
 
-        self.stdout.write(self.style.SUCCESS('Database abailable!'))
+        self.stdout.write(self.style.SUCCESS('Database available!'))
