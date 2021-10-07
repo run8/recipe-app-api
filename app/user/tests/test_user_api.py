518ea57d0925b0310c3a87d1aa1333f8ec268482
Tom Stagnaro <tomstagnaro1@gmail.com>
2021-10-07 13:14:42 -0700

    Fixing test pass even if pw is too short

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -24,8 +24,8 @@ class PublicUserApiTests(TestCase):
         """Test creating user with a valid payload is successful"""
         payload = {
             'email': 'test@gmail.com',
-            'password': 'testpass',
-            'name': 'Test name'
+            'password': 'pw',
+            'name': 'Test'
         }
         res = self.client.post(CREATE_USER_URL, payload)
 
@@ -36,7 +36,11 @@ class PublicUserApiTests(TestCase):
 
     def test_user_exists(self):
         """Test creating a user that already exists fails"""
-        payload = {'email': 'test@gmail.com', 'password': 'testpass'}
+        payload = {
+            'email': 'test@gmail.com',
+            'password': 'testpass',
+            'name': 'Test',
+        }
         create_user(**payload)
         res = self.client.post(CREATE_USER_URL, payload)
 
@@ -44,7 +48,11 @@ class PublicUserApiTests(TestCase):
 
     def test_password_too_short(self):
         """Test that the password must be more than 5 characters"""
-        payload = {'email': 'test@gmail.com', 'password': 'pw'}
+        payload = {
+            'email': 'test@gmail.com',
+            'password': 'pw',
+            'name': 'Test',
+        }
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
