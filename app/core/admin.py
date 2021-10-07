eebe0501ac5db103441bc0b18fecc1d5be06f8e0
Tom Stagnaro <tomstagnaro1@gmail.com>
2021-10-07 13:00:04 -0700

    47 Fixed linting issues various files. All tests pass.

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.utils.translation import gettext as _
+# from django.utils.translation import gettext as _
 
 from core import models
 
@@ -10,18 +10,18 @@ class UserAdmin(BaseUserAdmin):
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        ( ('Personal Info'), {'fields': ('name',)}),
+        (('Personal Info'), {'fields': ('name',)}),
         (
-             ('Permissions'),
+            ('Permissions'),
             {
                 'fields': (
                     'is_active',
                     'is_staff',
                     'is_superuser',
                 )
-            }
+             }
         ),
-        ( ('Important dates'), {'fields': ('last_login',)}),
+        (('Important dates'), {'fields': ('last_login',)}),
     )
     add_fieldsets = (
         (None, {
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
-from django.test import Client
+
 
 class AdminSiteTests(TestCase):
 
